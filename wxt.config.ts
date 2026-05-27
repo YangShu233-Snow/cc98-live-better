1a633a7282f464b5a8253b187e177840058e1ab0
Yangshu233 <750704076@qq.com>
2026-05-27 16:46:14 +0800

    feat: add extension icons

--- wxt.config.ts
+++ wxt.config.ts
@@ -18,7 +18,18 @@ export default defineConfig({
       "*://api.cc98.org/*",
       "*://file.cc98.org/*",
     ],
+    icons: {
+      '16': 'icon/16.png',
+      '32': 'icon/32.png',
+      '48': 'icon/48.png',
+      '128': 'icon/128.png',
+    },
     action: {
+      default_icon: {
+        '16': 'icon/16.png',
+        '24': 'icon/24.png',
+        '32': 'icon/32.png',
+      },
       default_popup: "popup/index.html",
       default_title: "CC98 Live Better",
     },
